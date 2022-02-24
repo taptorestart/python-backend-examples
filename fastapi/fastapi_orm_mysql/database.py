ba2ad48bc316a87930910955fb17fb6596384cc9
taptorestart <taptorestart@gmail.com>
2022-02-24 14:29:58 +0900

    CLN: Clean unnecessary info

--- fastapi/fastapi_orm_mysql/database.py
+++ fastapi/fastapi_orm_mysql/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database connection
-username = 'root'
-password = 'lo[rdb]-3216'
+username = 'admin'
+password = 'verysecret'
 host = 'localhost'
 port = 3306
-database = 'fastapi'
+database = 'your_database'
 
 SQLALCHEMY_DATABASE_URL = f'mysql://{username}:{password}@{host}:{port}/{database}'
 
