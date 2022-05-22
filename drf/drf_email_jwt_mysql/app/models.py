89eadb23953dd25bed5fd2196ca429644ea12a28
taptorestart <taptorestart@gmail.com>
2022-05-22 22:36:29 +0900

    ENH: Add a user model

--- drf/drf_email_jwt_mysql/app/models.py
+++ drf/drf_email_jwt_mysql/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.CharField(max_length=64, null=False, blank=False, unique=True)
+    password = models.CharField(max_length=255)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    # Set email as username_field
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        managed = True
+        db_table = 'user'
--- drf/drf_email_jwt_mysql/project/settings.py
+++ drf/drf_email_jwt_mysql/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app.User'
